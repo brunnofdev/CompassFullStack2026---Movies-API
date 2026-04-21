3b5dd7286e3f5141e54cf9e14558cd3d5f2b5cf3
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-04-21 20:56:27 -0300

    feat: implement express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Teste")
+import express from "express";
+import { AppDataSource } from "./database/data-source";
+import { errorMiddleware } from "./middlewares/error-middleware";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.info(`[${req.method}] ${req.url}`);
+  next();
+});
+
+//Routes here
+
+app.use(errorMiddleware);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.info("Database connected successfully!");
+    app.listen(3000, () => {
+      console.info("Server is running on http://localhost:3000");
+    });
+  })
+  .catch((error) => console.error("Database connection failed:", error));
