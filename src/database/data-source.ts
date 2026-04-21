a65e97c72b814acf346768937dfe9e020694b7f3
Brunno Felipe Bezerra <brunnofdev@gmail.com>
2026-04-21 17:16:12 -0300

    fix: change postgres port to 5433

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,15 +2,15 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "admin",
-    password: "adminpassword",
-    database: "movie_db",
-    synchronize: false, 
-    logging: true, 
-    entities: ["src/models/*.ts"], 
-    migrations: ["src/database/migrations/*.ts"],
-    subscribers: [],
+  type: "postgres",
+  host: "localhost",
+  port: 5433,
+  username: "admin",
+  password: "adminpassword",
+  database: "movie_db",
+  synchronize: false,
+  logging: true,
+  entities: ["src/models/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
+  subscribers: [],
 });
